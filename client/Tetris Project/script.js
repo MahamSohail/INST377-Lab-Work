098efff0eba8a270df11bd6373ea6387b993ef52
Maham Sohail <92546069+MahamSohail@users.noreply.github.com>
2022-09-29 18:07:24 -0400

    commit

--- client/Tetris Project/script.js
+++ client/Tetris Project/script.js
@@ -2,13 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
     const ScoreDisplay = document.querySelector('#score')
-    cnst StartBtn = document.querySelector('#start-button')
+    const StartBtn = document.querySelector('#start-button')
     const width = 10
-})
 
 //The Tetrimino 
 const lTetromino = [
-    [1, width+ 1, width=2+1, 2], 
+    [1, width+ 1, width*2+1, 2], 
     [width, width+1, width+2, width*2+2],
     [1,width+1, width*2+1,width*2],
     [width, width*2, width*2+1,width*2+2]
@@ -22,13 +21,16 @@ const zTetromino = [
 ]
 
 const tTetrominio = [
-    
+    [1, width, width+1, width+2],
+    [1,width+1, width+2, width*2+1],
+    [width,width+1,width+2, width*2+1],
+    [1,width,width+1, width*2+1]  
 ]
 
 const oTetromino = [
-    [0,1,width,width+1]
-    [0,1,width,width+1]
-    [0,1,width,width+1]
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
     [0,1,width,width+1]
 
 ]
@@ -39,3 +41,77 @@ const iTetromino = [
     [1, width+1, width*2+1,width*3+1],
     [width,width+1, width+2, width+3]
 ]
+
+const theTetrominoes = [lTetromino, zTetromino, tTetrominio,oTetromino, iTetromino]
+let currentPosition = 4
+let currentRotation = 0
+
+// randomly select 
+let random = Math.floor(Math.random()*theTetrominoes.length)
+console.log(random)
+let current = theTetrominoes[random][0]
+
+// draw the first rotation 
+function draw(){
+    current.forEach(index => {
+        squares[currentPosition + index]. classList.add('tetromino')
+    })
+}
+// undraw the random tetromino 
+function undraw(){
+    current.forEach(index => {
+        squares[currentPosition + index].classList.remove('tetromino')
+    })
+}
+// make the tetromino move down every second 
+timerId = setInterval(moveDown, 1000)
+
+//assign function to keyCodes
+function control(e){
+    if(e.keyCode ===37){
+        moveLeft() 
+    } else if (e.keyCode === 38){
+        /// rotate
+    } else if (e.keyCode === 39){
+        moveRight() 
+    } else if (e.keyCode == 40){
+        moveDown()
+    }
+}
+document.addEventListener('keyup',control)
+
+//move down function 
+function moveDown(){
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+}
+// freeze function 
+function freeze() {
+    if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+        current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+        //start a new tetromino falling 
+        random = Math.floor(Math.random() * theTetrominoes.length)
+        current = theTetrominoes[random][currentRotation]
+        currentPosition = 4
+        draw()
+    }
+}
+
+// move the tetromino left, unless is at the edge or there is a blockage
+function moveLeft(){
+    undraw()
+    const isAtLeftEdge = current.some(index => (currentPosition + index)% width === 0)
+    if (!isAtLeftEdge) currentPosition -= 1
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+        currentPosition +=1
+    }
+
+    draw()
+
+}
+
+// move the tetrmino right unless at the edge 
+
+})
