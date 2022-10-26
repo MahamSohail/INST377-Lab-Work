976454aa4b967c4e707d033a06105c84b2539964
Maham Sohail <92546069+MahamSohail@users.noreply.github.com>
2022-10-26 18:10:41 -0400

    commit

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -22,7 +22,7 @@ function processRestaurants(list) {
 }
 
 function filterList(array, filterInputValue) {
-  const newArray = array.filter(() => {
+  const newArray = array.filter((item) => {
     if (!item.name) { return; }
     const lowerCaseName = item.name.toLowerCase();
     const lowerCaseQuery = filterInputValue.toLowerCase();
