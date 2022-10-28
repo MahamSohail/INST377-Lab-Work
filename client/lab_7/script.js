79044f5c266b3093beeec6c159c5df7a720deceb
Maham Sohail <92546069+MahamSohail@users.noreply.github.com>
2022-10-28 12:29:00 -0400

    commit

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -31,25 +31,22 @@ function injectHTML(list) {
 }
 
 function processRestaurants(list) {
-}
-
-function filterList(array, filterInputValue) {
-  const newArray = array.filter((item) => {
-    if (!item.name) { return; }
-    const lowerCaseName = item.name.toLowerCase();
-    const lowerCaseQuery = filterInputValue.toLowerCase();
-    return lowerCaseName.includes(lowerCaseQuery);
+  console.log('fired restaurants list');
+  const range = [...Array(15).keys()];
+  const newArray = range.map((item) => {
+    const index = getRandomIntInclusive(0, list.length);
+    return list[index];
   });
-  // return newArray;
+  return newArray;
 }
 
 async function mainEvent() {
   /*
-      ## Main Event
+    ## Main Event
       Separating your main programming from your side functions will help you organize your thoughts
-        When you're not working in a heavily-commented "learning" file, this also is more legible
-        If you separate your work, when one piece is complete, you can save it and trust it
-    */
+      When you're not working in a heavily-commented "learning" file, this also is more legible
+      If you separate your work, when one piece is complete, you can save it and trust it
+  */
 
   // the async keyword means we can make API requests
 
@@ -62,15 +59,13 @@ async function mainEvent() {
   const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
 
   /*
-      Below this comment, we log out a table of all the results using "dot notation"
-      An alternate notation would be "bracket notation" - arrayFromJson["data"]
-      Dot notation is preferred in JS unless you have a good reason to use brackets
-The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 1,000 records we need
-    */
-  // console.log('Test PG County Set');
+    Below this comment, we log out a table of all the results using "dot notation"
+    An alternate notation would be "bracket notation" - arrayFromJson["data"]
+    Dot notation is preferred in JS unless you have a good reason to use brackets
+    The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 1,000 records we need
+  */
   console.table(arrayFromJson.data);
 
-  // eslint-disable-next-line max-len
   // in your browser console, try expanding this object to see what fields are available to work with
   // for example: arrayFromJson.data[0].name, etc
   console.log(arrayFromJson.data[0]);
@@ -79,47 +74,35 @@ The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 1,
   console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
 
   // This IF statement ensures we can't do anything if we don't have information yet
-  if (arrayFromJson.data?.length) { return; } // the question mark in this means "if this is set at all"
+  if (arrayFromJson.data?.length > 0) { // the question mark in this means "if this is set at all"
+    submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
 
-  submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
+    // Hide Load Button
+    loadAnimation.classList.remove('lds-ellipsis');
+    loadAnimation.classList.add('lds-ellipsis_hidden');
 
-  // Hide Load Button
-  loadAnimation.classList.remove('lds-ellipsis');
-  loadAnimation.classList.add('lds-ellipsis_hidden');
+    // And here's an eventListener! It's listening for a "submit" button specifically being clicked
+    // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
+    form.addEventListener('submit', (submitEvent) => {
+      // This is needed to stop our page from changing to a new URL even though it heard a GET request
+      submitEvent.preventDefault();
 
-  let currentList = [];
+      // This constant will have the value of your 15-restaurant collection when it processes
+      const restaurantList = processRestaurants(arrayFromJson.data);
+      console.log(restaurantList);
+      // And this function call will perform the "side effect" of injecting the HTML list for you
+      injectHTML(restaurantList);
 
-  form.addEventListener('input', (event) => {
-    console.log(event.target.value);
-    //const filteredList = filterList(currentList, event.target.value);
-    currentList = filterList(currentList, event.target.value);
-    injectHTML(currentList);
-  });
-
-  // And here's an eventListener! It's listening for a "submit" button specifically being clicked
-  // eslint-disable-next-line max-len
-  // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
-  form.addEventListener('submit', (submitEvent) => {
-    const restaurantList = processRestaurants(arrayFromJson.data);
-    submitEvent.preventDefault();
-
-    // eslint-disable-next-line max-len
-    // This is needed to stop our page from changing to a new URL even though it heard a GET request
-    // This constant will have the value of your 15-restaurant collection when it processes
-    currentList = processRestaurants(arrayFromJson.data);
-    console.log(currentList);
-    // And this function call will perform the "side effect" of injecting the HTML list for you
-    injectHTML(currentList);
-
-    // By separating the functions, we open the possibility of regenerating the list
-    // without having to retrieve fresh data every time
-    // We also have access to some form values, so we could filter the list based on name
-  });
+      // By separating the functions, we open the possibility of regenerating the list
+      // without having to retrieve fresh data every time
+      // We also have access to some form values, so we could filter the list based on name
+    });
+  }
 }
 
 /*
-    This last line actually runs first!
-    It's calling the 'mainEvent' function at line 57
-    It runs first because the listener is set to when your HTML content has loaded
-  */
+  This last line actually runs first!
+  It's calling the 'mainEvent' function at line 57
+  It runs first because the listener is set to when your HTML content has loaded
+*/
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
